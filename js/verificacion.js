584392a7d6a90f59aba4162c035d0c455c1dd725
sigsant <sigfridsantamera@hotmail.com>
2018-11-21 21:10:00 +0100

    mostrar-ocultar pie-imagen

--- js/verificacion.js
+++ js/verificacion.js
@@ -1,9 +1,51 @@
 import { Expresiones } from "./clases.js";
 
+/* Aparece el logo de visa a partir de una máscara: 
+ * 
+ *  XXXX XXXX XXXX XXXX
+ */
+
+document.querySelector("#card-number").onkeyup = () => {
+    let regex        = new RegExp('^\\d{4}\\s+\\d{4}\\s+\\d{4}\\s+\\d{4}$');
+    let campoTarjeta = document.querySelector("#card-number");
+    
+     (regex.test(campoTarjeta.value)) ? 
+         campoTarjeta.classList.add('visa'): 
+         campoTarjeta.classList.remove('visa');
+};
+
+// Oculta los divs de eleccion de compra
+
+const ocultar = bloque =>{
+    document.querySelector(bloque).style.display = "none";
+};
+
+// Muestra en bloque los divs de eleccion de compra
+
+const mostrar = bloque =>{
+    document.querySelector(bloque).style.display = "block";
+};
+
+document.querySelector('#paypal').onclick = () =>{
+    ocultar("#pay-card");
+    ocultar('.fig-card');
+    mostrar('#pay-paypal');
+    mostrar('.fig-pay');
+};
+
+document.querySelector('#visa').onclick = () =>{
+    ocultar('#pay-paypal');
+    ocultar('.fig-pay');
+    mostrar('#pay-card');
+    mostrar('.fig-card');
+};
+
+document.querySelector('button').onclick = validar;
+
 // TODO Refactorizar
 // Llama a la clase que contiene la validación mediante Regex
 
-let validar = () => {
+function validar(){
     let   nombre         = document.querySelector('#nombre');
     const nombreError    = document.querySelector('.nombre');
     let   apellido       = document.querySelector('#apellido');
@@ -25,31 +67,4 @@ let validar = () => {
     validacion.validarnombre(provincia, provinciaError);
     validacion.validarZip(zipCode, zipCodeError);
     validacion.validarDir(direccion, direccionError);
-};
-
-document.querySelector('button').onclick = validar;
-
-/* Aparece el logo de visa a partir de una máscara: 
- * 
- *  XXXX XXXX XXXX XXXX
- */
-
-document.querySelector("#card-number").onkeyup = () => {
-    let regex        = new RegExp('^\\d{4}\\s+\\d{4}\\s+\\d{4}\\s+\\d{4}$');
-    let campoTarjeta = document.querySelector("#card-number");
-    
-     (regex.test(campoTarjeta.value)) ? 
-         campoTarjeta.classList.add('visa'): 
-         campoTarjeta.classList.remove('visa');
-};
-
-document.querySelector('#paypal').onclick = () =>{
-    document.querySelector('#pay-card').style.display   = "none";
-    document.querySelector('#pay-paypal').style.display = "block";
-};
-
-document.querySelector('#visa').onclick = () =>{
-    document.querySelector('#pay-paypal').style.display = "none";
-    document.querySelector('#pay-card').style.display   = "block";
-};
-
+}
